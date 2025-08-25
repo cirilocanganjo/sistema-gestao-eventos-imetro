3b8f2a86ebf8705f299447adc9043f8a6dea0e9c
cirilocanganjo <cirilocanganjolr@gmail.com>
2025-08-25 12:39:22 +0100

    componente de login criado

--- routes/auth/routes.php
+++ routes/auth/routes.php
@@ -1,1 +1,6 @@
 <?php
+use Illuminate\Support\Facades\Route;
+
+Route::prefix("sgei")->group(function () {
+    Route::get('/login', \App\Livewire\Login\LoginComponent::class)->name('login');
+});
--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,4 @@
 require __DIR__."/home/routes.php";
 require __DIR__."/users/routes.php";
 require __DIR__."/dashboard/routes.php";
+require __DIR__."/auth/routes.php";
